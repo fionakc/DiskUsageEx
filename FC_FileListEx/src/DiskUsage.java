c47b2854d9946367afb51fe55488761dffaa3056
crookfion <crookfion@gs1608.AD.ECS.VUW.AC.NZ>
2018-08-01 14:29:32 +1200

    DiskUsageEx - mostly complete
    The total file size is being calculated correctly. There is some
    internet code that works properly to list any files with a given
    extension, but it is not working correctly with accessing subfolders.

--- FC_FileListEx/src/DiskUsage.java
+++ FC_FileListEx/src/DiskUsage.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FilenameFilter;
+
 import ecs100.*;
 
 public class DiskUsage {
@@ -7,9 +9,6 @@ public class DiskUsage {
 	String pathDir="C:\\Users\\crookfion\\Documents\\Swen501 slides";
 	File file;
 	long totalSize=0;
-	//returns the size of the ordinary file named by path as an int
-	//new File(path).length()    
-	//File f=new File(C:/Users/crookfion/Documents/Swen501_slides);
 	
 	public DiskUsage() {
 		//this.path=UIFileChooser.open();
@@ -26,6 +25,10 @@ public class DiskUsage {
 	return new File(filePath).listFiles(); // returns an array of File objects 
 		//representing the files in the directory named by path
 		
+	}
+	
+	public File[] fileFilter(String filePath, String ext) {
+		return new File(filePath).listFiles(new Filter(ext));
 	}
 	
 	public boolean hasDirectory(String filePath) {
@@ -51,5 +54,86 @@ public class DiskUsage {
 		
 		return totalSize;
 	} //end totalDirSize
+	
+	//Add another mode that reports the size of each subdirectory 
+	//and all its children individually, showing the hierarchy of directories.
+	
+	//Add another mode that finds all files within a directory tree 
+	//with a particular extension (e.g. .java or .class) and lists them.
+	//use filenamefilter
+	public void fileNameFilter(String filePath, String ext) {
+			
+		System.out.println("into fileNameFilter");
+		//File[] fileArray=fileFilter(filePath,ext);       //creating array of size 0
+		File[] fileArray = new File(filePath).listFiles(new Filter(ext));
+		System.out.println("created array");
+		System.out.println(fileArray.length);
+		for(int i=0;i<fileArray.length;i++) { //loop through array
+			if(hasDirectory(fileArray[i].toString())){	//if array element is directory
+				fileNameFilter(fileArray[i].toString(),ext);	//access files inside
+			} else {
+				System.out.println(fileArray[i].toString());	//print out file path
+			}
+			//System.out.println("into array loop");
+			//System.out.println(fileArray[i].toString());
+		}
+		System.out.println("passed loop");
+	
+		
+//		File[] fileArray=fileList(filePath);  //generate fileArray from passed path
+//		for(int i=0;i<fileArray.length;i++) {  //loop through array
+//			if(hasDirectory(fileArray[i].toString())) {  //if array element is directory
+//				totalDirSize(fileArray[i].toString());	//want to list file paths inside
+//			}else {
+//				//totalSize+=1;
+//				totalSize+=fileLength(fileArray[i].toString()); //add up file size
+//				System.out.println(fileArray[i].toString()+","+totalSize); //otherwise print path name
+//			} //end else
+//		} //end for
+	}
+	
+	/**from the internet*/
+	//this code works
+	public void findFiles(String dir, String ext) {
+		File file = new File(dir);
+		if (!file.exists())
+			System.out.println(dir + " Directory doesn't exists");
+		File[] listFiles = file.listFiles(new Filter(ext));
+		// File[] listFiles = file.listFiles((d, s) -> {
+		// return s.toLowerCase().endsWith(ext);
+		// });
+
+		if (listFiles.length == 0) {
+			System.out.println(dir + "doesn't have any file with extension " + ext);
+		} else {
+			for (File f : listFiles)
+				System.out.println("File: " + dir + File.separator + f.getName());
+		}
+	}
+	
+	
+	// FileNameFilter implementation
+	public static class Filter implements FilenameFilter {
+		//System.out.println("into Filter");
+		private String ext;
+
+		public Filter(String ext) {
+			this.ext = ext.toLowerCase();
+			System.out.println("into Filter constructor");
+		}
+
+		@Override
+		public boolean accept(File fileName, String name) {
+			//return name.toLowerCase().endsWith(ext);
+			//System.out.println("into Filter accept");
+			if (name.endsWith(ext)) {
+	            return true;
+	        } 				        
+			return false;
+
+		}
+
+	}
+	
 	
 }
--- FC_FileListEx/src/test.java
+++ FC_FileListEx/src/test.java
@@ -2,13 +2,16 @@
 public class test {
 
 	public static void main(String[] args) {
-		String pathDir="C:\\Users\\crookfion\\Documents\\Swen501 slides\\second folder";
+		String pathDir="C:\\Users\\crookfion\\Documents\\Swen501 slides\\smaller folder";
+		String ext=".txt";
 		
 		DiskUsage disk=new DiskUsage();
 		//System.out.println(disk.fileLength());
 		//System.out.println(disk.hasDirectory());
-		long fileSizes=disk.totalDirSize(pathDir);
-		System.out.println("The total size of all files in "+pathDir+" is "+fileSizes+" bytes");
+		//long fileSizes=disk.totalDirSize(pathDir);
+		//System.out.println("The total size of all files in "+pathDir+" is "+fileSizes+" bytes");
+		//disk.fileNameFilter(pathDir,ext);
+		disk.findFiles(pathDir,ext);
 		
 	}
 	
